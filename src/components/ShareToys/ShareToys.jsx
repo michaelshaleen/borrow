63fc833954343aa92f353737ae8ac25ce1c220d6
michaelshaleen <shaleenmichael@yahoo.com>
2021-03-15 13:41:42 -0500

    skeleton for share new toys

--- src/components/ShareToys/ShareToys.jsx
+++ src/components/ShareToys/ShareToys.jsx
@@ -1,7 +1,21 @@
 function ShareToys() {
 
   return(
-    <p>Share Toys</p>
+    <>
+    <p>Share New Toys</p>
+    <input
+    type="text"
+    placeholder="description"
+    />
+    <select name="Age Group">
+      <option value="1">0-3 Years Old</option>
+      <option value="2">3-5 Years Old</option>
+      <option value="3">5-10 Years Old</option>
+    </select>
+    <button>Share Toy</button>
+    <button>Cancel</button>
+
+    </>
   )
 
 }
