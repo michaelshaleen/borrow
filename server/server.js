fc59a8345279192a03767fe5926e0f4058b9bc0d
michaelshaleen <shaleenmichael@yahoo.com>
2021-03-29 16:03:35 -0500

    fixing errors

--- server/server.js
+++ server/server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 require('dotenv').config();
-const fileUpload = require('express-fileUpload');
+const fileUpload = require('express-fileupload');
 
 const app = express();
 
